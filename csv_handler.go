4bcd197d51cadaa1edabf529797c554c29b94ffb
Brian Chau <brian.chau@outlook.com>
2021-08-15 17:24:07 -0700

    Tailored it to look for specific column names for my specific test, reducing run time down from 18 seconds to just under 5 seconds

--- csv_handler.go
+++ csv_handler.go
@@ -9,22 +9,7 @@ import (
     "strconv"
 )
 
-func check_condition( csv_record map[string]string ) (pu_count int, do_count int) {
-    payment_type       := csv_record["payment_type"]
-    pu_location        := csv_record["PULocationID"]
-    do_location        := csv_record["DOLocationID"]
-    pu_count, do_count  = 0, 0
-    if payment_type == "3" {
-        if pu_location == "170" {
-            pu_count += 1
-        } else if do_location == "170" {
-            do_count += 1
-        }
-    }
-    return pu_count, do_count
-}
-
-func process_csv_line_by_line( filePath string, check_condition func( map[string]string ) (int, int) ) {
+func process_csv_line_by_line( filePath string ) {
     f, err := os.Open(filePath)
     if err != nil {
         log.Fatal("Unable to read input file: " + filePath, err)
@@ -34,6 +19,8 @@ func process_csv_line_by_line( filePath string, check_condition func( map[string
     r := csv.NewReader(f)
     var header []string
     pu_count, do_count := 0, 0
+    payment_type_index := 0
+    pu_index, do_index := 0, 0
     for {
         record, err := r.Read()
         if err == io.EOF {
@@ -46,14 +33,23 @@ func process_csv_line_by_line( filePath string, check_condition func( map[string
         }
         if header == nil {
             header = record
-        } else {
-            csv_record := map[string]string{}
             for i := range header {
-                csv_record[header[i]] = record[i]
+                if header[i] == "payment_type" {
+                    payment_type_index = i;
+                } else if header[i] == "PULocationID" {
+                    pu_index           = i;
+                } else if header[i] == "DOLocationID" {
+                    do_index           = i;
+                }
             }
-            pu_, do_ := check_condition( csv_record )
-            pu_count += pu_
-            do_count += do_
+        } else {
+            if record[payment_type_index] == "3" {
+                if record[pu_index] == "170" {
+                    pu_count += 1
+                } else if record[do_index] == "170" {
+                    do_count += 1
+                }
+            }
         }
     }
 
@@ -65,5 +61,5 @@ func main() {
         log.Fatal("Too few arguments.")
     }
     filename := os.Args[1]
-    process_csv_line_by_line( filename, check_condition )
+    process_csv_line_by_line( filename )
 }
